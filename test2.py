a193969363b0c47881a19fa55565cda6d217fab3
Lavanya_github <lavanya.n28@gmail.com>
2026-02-26 12:09:02 +0530

    Update test2.py
    WIP: new line added to test the merge changes from main to lavanya branch

--- test2.py
+++ test2.py
@@ -3,3 +3,5 @@ print("another new file to check merge feature")
 print("newly added line")
 
 print("adding a new line to test fetch/merge/pull concepts")
+
+print("new line added to test the merge changes from main to lavanya branch")
